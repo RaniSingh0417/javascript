53d6f89da1f89e0f2fb4481221c0725c2135a916
Rani Singh <rs170504singh@gmail.com>
2022-12-08 10:15:09 +0530

    adding files

---
+++ array.js
@@ -0,0 +1,20 @@
+// array
+let a=["post 1","post 2", "post 3","post 4","post 5","post 6"];
+// accessing values of array
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+// for(let i=0;i<6;i=i+1)
+// getting length of array
+console.log(a.length);
+// adding an element in array
+a.push("post 7");
+a.push("post 8");
+console.log(a);
+// removing an element of array
+a.pop();
+a.pop();
+console.log(a);
---
+++ loops.js
@@ -0,0 +1,4 @@
+for(let i=0;i<8;i=i+1){
+    console.log("inside content of for loop");
+}
+console.log("out of for loop");
