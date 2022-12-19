5858875d702fd9de7d020295a7915cbb096a106f
Rani Singh <rs170504singh@gmail.com>
2022-12-19 12:21:05 +0530

    object,mapping,arrayfind

--- forofloop.js
+++ forofloop.js
@@ -1,13 +1,35 @@
 // for of loop is a special kind of for loop which is used 
 // specially in case of array or similar data type
-const arr=["val 1","val 2","val 3","val 4"];
-for(let i=0;i<4;i++){
-    console.log(arr[i]);
-}
+// const arr=["val 1","val 2","val 3","val 4"];
+// for(let i=0;i<4;i++){
+//     console.log(arr[i]);
+// }
 
-// a similar kind of short for of loop can be achieved to do the same task
-for(let a of arr){
-    console.log(a);
-}
+// // a similar kind of short for of loop can be achieved to do the same task
+// for(let a of arr){
+//     console.log(a);
+// }
 // the variable a starts from first index of array.
 // by updating its value to next element one by one.
+// for of loop is similar as array mapping and array find.
+const practicearr=[
+    "Element 1",
+    "Element 2",
+    "Element 3",
+    "Element 4",
+    "Element 5",
+    "Element 6",
+    "Element 7"
+]
+// for(let candidate of practicearr){
+//     console.log(candidate,"of for of loop");
+// }
+// // array mapping
+// practicearr.map((candidate)=>{
+// console.log(candidate,"of mapping");
+// });
+// array find
+const findelement = practicearr.find((candidate)=>{
+   return candidate==="Element 4";
+});
+console.log(findelement);
---
+++ object.js
@@ -0,0 +1,41 @@
+// const tutorialcopy10rs={
+//      name:"Rani Singh",
+// branch:"ECE",
+// rollno:"2021041157",
+// session:"2021-2025",
+// college:"MMMUT,GKp"
+// };
+// // whenever want to store data which is in the form of key value pairs,then
+// // object is used
+// console.log(tutorialcopy10rs);
+// // keys should never have spaces btw them and should not start with
+// // any number or any special characters
+// // if it has spaces,then key should be enclosed within parenthesis
+// // accessing value of object in two ways
+// // first way
+// console.log(tutorialcopy10rs["name"]);
+// console.log(tutorialcopy10rs["college"]);
+// // second way
+// console.log(tutorialcopy10rs.name);
+// const wondersofworld={
+//     first: "Taj Mahal",
+//     second:"great wall of china",
+//     third:"victoria falls",
+//     fourth:"grand canyon",
+//     fifth:"eiffel tower"
+
+// }
+// console.log(wondersofworld["first"]);
+// console.log(wondersofworld["second"]);
+// console.log(wondersofworld.third);
+// console.log(wondersofworld.fourth);
+// to get all the keys of an object
+//example 3
+const details={
+    name:"rani singh",
+    dob:"17/12/2003",
+    phonenumber:"666666666"
+}
+console.log(details["dob"]);
+console.log(details.name);
+
