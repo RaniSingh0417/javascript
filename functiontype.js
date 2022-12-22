6c6e1b435b76285ba335b029567e0b3b00c2e887
Rani Singh <rs170504singh@gmail.com>
2022-12-22 20:03:54 +0530

    msg

---
+++ functiontype.js
@@ -0,0 +1,20 @@
+// functions are of two types on the basis of returning property
+// 1.Non-Returning functions,which just perform the task assigned and did not return anything
+// 2.Returning functions are those which perform the task assigned and return something
+
+
+
+// returning functions ends with the keyword "return data"
+// whereas non-returning doesn't return anything
+// 
+
+// while calling returning functions we neeed to store the data in another variable
+// whereas non-returning doesn't need anything
+const returnfunction1 = () =>{
+    let a=5;
+    let b=20;
+    let c=a+b;
+    return c;
+};
+let resultofreturningfunc=returnfunction1();
+console.log(resultofreturningfunc);
