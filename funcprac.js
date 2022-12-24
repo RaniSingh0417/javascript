4555c30673996f0a23e9b882510708d9e24eea64
Rani Singh <rs170504singh@gmail.com>
2022-12-24 17:03:55 +0530

    msg

---
+++ funcprac.js
@@ -0,0 +1,26 @@
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// // console.log("Average of a and b is ",1+(a+b)/2);
+// // console.log("Average of c and b is ",(c+b)/2);
+// // console.log("Average of a and c is ",(a+c)/2);
+
+
+// // Math.round is used to roundoff any number in Js
+
+// // function can be define in two ways
+// // first way(arrow function) and this is used in latest time
+// const avg = (x,y) =>{
+// console.log("Done");
+// return Math.round((x+y)/2);
+// }
+// console.log(avg(9,8));
+
+
+// // 2nd way
+// function Avg(x,y){
+//     console.log("Done");
+//     return Math.round((x+y)/2);
+    
+//     }
+// console.log(Avg(a,b));
