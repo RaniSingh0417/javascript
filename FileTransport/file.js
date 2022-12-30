beddbbf47738cf0d5a350eb8e6020f89a8df8bb3
Rani Singh <rs170504singh@gmail.com>
2022-12-30 17:22:36 +0530

    msg

--- FileTransport/file.js
+++ FileTransport/file.js
@@ -1,5 +1,14 @@
 const add = (a, b) => {
   return a + b;
 };
+const sub = (a, b) => {
+  return a - b;
+};
+const mul = (a, b) => {
+  return a * b;
+};
 // exporting statement is used
-module.exports = { add };
+// export default { add };
+module.exports = { add, sub, mul };
+// module.exports={sub};
+// module.exports={mul};
--- FileTransport/main.js
+++ FileTransport/main.js
@@ -2,6 +2,15 @@
 // in our file ,which is present in anyother file
 // then we can import that code from that file,making sure that
 // file is ready to export
+
+
+
 // importing statement  where file is to be exported(sender)
-const { add } = require("./file");
-console.log(add(3, 4));
+
+const { add,sub,mul } =require("./file");
+const sum=add(3,7);
+console.log(sum);
+const diff=sub(7,3);
+console.log(diff);
+const multiply=mul(6,7);
+console.log(multiply);
