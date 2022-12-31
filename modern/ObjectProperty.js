f65e910b88f94a151e7e7dd59c7745424382cf7c
Rani Singh <rs170504singh@gmail.com>
2022-12-31 22:46:10 +0530

    practice

--- modern/ObjectProperty.js
+++ modern/ObjectProperty.js
@@ -8,14 +8,14 @@ console.log(normalobject);
 // in modern javascript, ES6(et myscript)
 // if in an object, the names of key and its is same
 // then we need not to mention both
-let icecream="vanilla";
-let cake="Blue-berry Cake";
-let sweets="rasmalai";
-let food="junkfood";
-let Foodie={
+let icecream = "vanilla";
+let cake = "Blue-berry Cake";
+let sweets = "rasmalai";
+let food = "junkfood";
+let Foodie = {
   icecream,
   cake,
   sweets,
-  food
+  food,
 };
 console.log(Foodie);
